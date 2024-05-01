d609f9729206bcaaeb0f6630ca0302c9bc1af055
Dezerack <114543331+Dezerack@users.noreply.github.com>
2024-04-30 17:45:06 -0700

    added the path

--- Assets/Scripts/SealBoss.cs
+++ Assets/Scripts/SealBoss.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SealBoss : MonoBehaviour
 {
     private bool CanBeHit = false;
+    private bool IsDiving = false;
+    public float sealSpeed;
 
     void Start()
     {
@@ -13,6 +15,30 @@ public class SealBoss : MonoBehaviour
 
     void Update()
     {
-        
+        if(IsDiving)
+        {
+            sealSpeed = 10;
+            CanBeHit = false;
+        }
+        else
+        {
+            sealSpeed = 2;
+            CanBeHit = true;
+        }
     }
+
+
+    public void Diving()
+    {
+        IsDiving = true;
+    }
+
+
+    /*
+    apply vertical movement up wait then down changing animation have a random number thing from 1-3 for amount of times going up until splash down
+    on splash down have damage radius and summon two small enemies
+    slow movement while not diving but quick movement while diving along with not being able to be hit
+
+    */
+
 }
