0209316c38e9dce0e45c3d36b3396b5b18363ad6
Dezerack <114543331+Dezerack@users.noreply.github.com>
2024-04-30 17:11:36 -0700

    decoration w/o coliders

---
+++ Assets/Scripts/SealBoss.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealBoss : MonoBehaviour
+{
+    private bool CanBeHit = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
