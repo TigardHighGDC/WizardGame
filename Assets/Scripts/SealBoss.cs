b148138f6dd1027e69845e571b7f7a49fe30c9fe
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2024-04-30 18:31:01 -0700

    Clang

--- Assets/Scripts/SealBoss.cs
+++ Assets/Scripts/SealBoss.cs
@@ -10,12 +10,11 @@ public class SealBoss : MonoBehaviour
 
     void Start()
     {
-        
     }
 
     void Update()
     {
-        if(IsDiving)
+        if (IsDiving)
         {
             sealSpeed = 10;
             CanBeHit = false;
@@ -27,18 +26,15 @@ public class SealBoss : MonoBehaviour
         }
     }
 
-
     public void Diving()
     {
         IsDiving = true;
     }
 
-
     /*
-    apply vertical movement up wait then down changing animation have a random number thing from 1-3 for amount of times going up until splash down
-    on splash down have damage radius and summon two small enemies
-    slow movement while not diving but quick movement while diving along with not being able to be hit
+    apply vertical movement up wait then down changing animation have a random number thing from 1-3 for amount of times
+    going up until splash down on splash down have damage radius and summon two small enemies slow movement while not
+    diving but quick movement while diving along with not being able to be hit
 
     */
-
 }
