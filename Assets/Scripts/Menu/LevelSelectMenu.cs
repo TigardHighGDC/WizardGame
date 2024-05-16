a55d9e5693230ca29e475bb7855fe1a8f417ff45
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2024-05-15 19:55:54 -0700

    Title & death (#59)
    * should be fixed
    
    * forgot one tree
    
    * Hope nothing breaks :)
    
    * fixed trees
    
    * added some small things
    
    * Made king room
    
    * Clang Ran
    
    * Update EditorUserSettings.asset
    
    * Better Organization
    
    ---------
    
    Co-authored-by: Dezerack <114543331+Dezerack@users.noreply.github.com>

---
+++ Assets/Scripts/Menu/LevelSelectMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectMenu : MonoBehaviour
+{
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnAirIslandButton()
+    {
+        SceneManager.LoadScene("AirIslandTileMap");
+    }
+
+    public void OnAirIslandBossButton()
+    {
+        SceneManager.LoadScene("AirFactory");
+    }
+
+    public void OnFireIslandButton()
+    {
+        SceneManager.LoadScene("FireIslandTileMap");
+    }
+
+    public void OnFireIslandBossButton()
+    {
+        SceneManager.LoadScene("FireColosseum");
+    }
+
+    public void OnWaterIslandBossButton()
+    {
+        SceneManager.LoadScene("SealFight");
+    }
+
+    public void OnMainIslandFightButton()
+    {
+        SceneManager.LoadScene("TBD");
+    }
+
+    public void OnMainIslandButton()
+    {
+        SceneManager.LoadScene("MainIsland");
+    }
+
+    public void OnFinalBossIslandButton()
+    {
+        SceneManager.LoadScene("TBD");
+    }
+}
